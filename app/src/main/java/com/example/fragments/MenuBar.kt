e2533b5d44ea9a49d82618ef55d148fbd8e1a91d
Tristan Stavast <104350793+TristanStavast@users.noreply.github.com>
2024-05-14 08:45:59 +0200

    Initial Commit 1

---
+++ app/src/main/java/com/example/fragments/MenuBar.kt
@@ -0,0 +1,34 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+private val Frag1 = FirstFragment()
+private val Frag2 = SecondFragment()
+class MenuBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_bar2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    private fun replaceFrag(fragment: Fragment)
+    {
+        if(fragment != null)
+        {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frameLayout, fragment)
+            transaction.commit()
+        }
+    }
+}
